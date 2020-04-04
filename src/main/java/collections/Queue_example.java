44fb3fc60b6310ba09a8dc0f1c1e8baee8f2992b
CrushPower <asovin.e.v@gmail.com>
2020-04-04 18:58:16 +0300

    Update

---
+++ src/main/java/collections/Queue_example.java
@@ -0,0 +1,41 @@
+package collections;
+
+import java.util.*;
+
+public class Queue_example {
+    //    create timer
+    public static void timerExample() throws InterruptedException {
+        int time = 10;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = time; i >= 0; i--)
+            queue.add(i);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+            Thread.sleep(1000);
+        }
+    }
+
+    //  create method for sort collections
+    static <E> List<E> heapSort(Collection<E> c) {
+        Queue<E> queue = new PriorityQueue<>(c);
+        List<E> result = new ArrayList<>();
+        while (!queue.isEmpty())
+            result.add(queue.remove());
+        return result;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        LinkedList<String> linkedListExample = new LinkedList<>();
+        linkedListExample.add("asd1");
+        linkedListExample.add("asd2");
+        linkedListExample.add("asd3");
+        linkedListExample.add("asd4");
+        linkedListExample.add("asd5");
+        linkedListExample.add("asd3");
+        linkedListExample.add("asd4");
+        linkedListExample.add("asd5");
+        System.out.println(linkedListExample);
+        System.out.println(heapSort(linkedListExample));
+        timerExample();
+    }
+}
