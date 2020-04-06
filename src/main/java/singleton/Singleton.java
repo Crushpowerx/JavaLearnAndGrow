f5716a3244d2c393ac2247570be54e55682120d0
CrushPower <asovin.e.v@gmail.com>
2020-04-06 11:38:16 +0300

    Update

--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -3,22 +3,23 @@ package singleton;
 public class Singleton {
     private static Singleton instance;
     private Singleton(int i){
-        System.out.println("Singleton " + i);
+        System.out.println("Singleton instance " + i);
     }
-    public static void getInstance(int i){
+    public static Singleton getInstance(int i){
         if (instance == null){
             instance = new Singleton(i);
-            System.out.println("Created instance!!!");
         } else {
             System.out.println("Only one instance!!!");
+            return instance;
         }
+        return instance;
     }
 }
 
-class test {
+class TestSingleton {
     public static void main(String[] args) {
-        Singleton.getInstance(1);
-        Singleton.getInstance(2);
-        Singleton.getInstance(3);
+        System.out.println("Return instance: " + Singleton.getInstance(1));
+        System.out.println("Return instance: " + Singleton.getInstance(2));
+        System.out.println("Return instance: " + Singleton.getInstance(3));
     }
 }
