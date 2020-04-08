5ea82f63fdb4de21a443a0ef61eefc4d77e5c2f1
CrushPower <asovin.e.v@gmail.com>
2020-04-08 19:02:07 +0300

    Update

---
+++ src/main/java/Other/ConstructorExample.java
@@ -0,0 +1,17 @@
+package Other;
+
+public class ConstructorExample {
+    String name;
+    int age;
+
+    public ConstructorExample(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        ConstructorExample constructorExample = new ConstructorExample("Name", 10);
+        System.out.println(constructorExample.name);
+        System.out.println(constructorExample.age);
+    }
+}
